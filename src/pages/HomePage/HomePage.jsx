b0d16cb25ce6c140a6475169e49c9097273c892d
kolya8077 <kolya8077@gmail.com>
2023-03-26 20:11:13 +0300

    day-1

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,31 +1,24 @@
-import { useState, useEffect } from 'react';
-import { Trending } from 'components/trending/Trending';
-import { fetchTrending } from 'servise/api';
-import { useLocation } from 'react-router-dom';
-import { Title } from 'components/trending/trending.styled';
+// import { useState, useEffect } from 'react';
+// import { Trending } from 'components/trending/Trending';
+// import { fetchTrending } from 'servise/api';
+// import { useLocation } from 'react-router-dom';
 import { Loading } from 'components/loading.style';
 
 const HomePage = () => {
-  const [trendingFilms, setTrendingFilms] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
+  // const [trendingFilms, setTrendingFilms] = useState([]);
+  // const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(() => {
-    fetchTrending().then(setTrendingFilms);
-    setIsLoading(true);
-  }, []);
+  // useEffect(() => {
+  //   fetchTrending().then(setTrendingFilms);
+  //   setIsLoading(true);
+  // }, []);
 
-  const location = useLocation();
+  // const location = useLocation();
 
   return (
     <>
       <div>
-        <Title>В тренді сьогодні</Title>
-
-        {isLoading ? (
-          <Trending trendingFilms={trendingFilms} location={location} />
-        ) : (
           <Loading> Завантаження... </Loading>
-        )}
       </div>
     </>
   );
