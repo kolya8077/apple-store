dad006fcfed551866897d6f98e696337d0157eb3
kolya8077 <kolya8077@gmail.com>
2023-03-26 12:28:28 +0300

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,32 +1,11 @@
-import { lazy } from 'react';
-import { HeaderEl } from './header/HeaderEl';
-import { Route, Routes } from 'react-router-dom';
-import Cast from 'components/cast/Cast';
-import Reviews from './reviews/Reviews';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { NotFound } from './NotFound/NotFound';
 
-const HomePage = lazy(() => import('pages/HomePage/HomePage'));
-const MoviePage = lazy(() => import('pages/MoviePage/MoviePage'));
-const FilmDetailsPage = lazy(() =>
-  import('pages/FilmDetailsPage/FilmDetailsPage')
-);
-
 export const App = () => {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<HeaderEl />}>
-          <Route index element={<HomePage />} />
-          <Route path="movies" element={<MoviePage />} />
-          <Route path="movies/:movieId" element={<FilmDetailsPage />}>
-            <Route path="cast" element={<Cast />} />
-            <Route path="revievs" element={<Reviews />} />
-          </Route>
-          <Route path='*' element={<NotFound />} />
-        </Route>
-      </Routes>
+      <NotFound />
       <ToastContainer theme="colored" position="top-right" autoClose={3000} />
     </>
   );
--- src/servise/api.js
+++ src/servise/api.js
@@ -11,34 +11,3 @@ export const fetchTrending = async () => {
   return response.data.results;
 };
 
-export const fetchDetails = async movie_id => {
-  const response = await axios.get(
-    `${BASE_URL}/3/movie/${movie_id}?api_key=${API_KEY}&language=uk-UA`
-  );
-
-  return response.data;
-};
-
-export const fetchCast = async movie_id => {
-  const response = await axios.get(
-    `${BASE_URL}/3/movie/${movie_id}/credits?api_key=${API_KEY}&language=uk-UA`
-  );
-
-  return response.data.cast;
-};
-
-export const fetchReviews = async movie_id => {
-  const response = await axios.get(
-    `${BASE_URL}/3/movie/${movie_id}/reviews?api_key=${API_KEY}&language=uk-UA&page=1`
-  );
-
-  return response.data.results;
-};
-
-export const fetchSearch = async query => {
-  const response = await axios.get(
-    `${BASE_URL}/3/search/movie?api_key=${API_KEY}&language=uk-UA&query=${query}&page=1&include_adult=false`
-  );
-
-  return response.data.results;
-};
