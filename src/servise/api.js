f2a92fd8a0e4fb14fea2922ae50655453819a95c
kolya8077 <kolya8077@gmail.com>
2023-03-26 12:43:29 +0300

    fix start

--- src/servise/api.js
+++ src/servise/api.js
@@ -3,11 +3,11 @@ import axios from 'axios';
 const API_KEY = '1f2d7fc7209a3cc852325c2be14de280';
 const BASE_URL = 'https://api.themoviedb.org';
 
-// export const fetchTrending = async () => {
-//   const response = await axios.get(
-//     `${BASE_URL}/3/trending/movie/day?api_key=${API_KEY}&language=uk-UA`
-//   );
+export const fetchTrending = async () => {
+  const response = await axios.get(
+    `${BASE_URL}/3/trending/movie/day?api_key=${API_KEY}&language=uk-UA`
+  );
 
-//   return response.data.results;
-// };
+  return response.data.results;
+};
 
